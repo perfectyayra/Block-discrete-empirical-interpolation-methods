cf23123686505c11999a1a40014f0934ce17d6d1
Perfect Yayra Gidisu <36855498+perfectyayra@users.noreply.github.com>
2022-08-03 19:05:44 +0200

    Update and rename cur_deim_block_maxvol.m to block_deim_maxvol.m

--- block_deim_maxvol.m
+++ block_deim_maxvol.m
@@ -1,4 +1,12 @@
-function irow = cur_deim_block_maxvol(U,k,p,tol)
+function irow = block_deim_maxvol(U,k,p,tol)
+% U is singular vectors
+% k desired number of indices
+% p block size
+% tol stopping cirteria for maxvol algorithm 
+
+% Revision date: June 18, 2022
+% (C) Perfect Gidisu, Michiel Hochstenbach 2022
+
 if nargin < 3 || isempty(p), p = 2; end
 if nargin < 4 || isempty(tol), tol = 2e-2; end
 
