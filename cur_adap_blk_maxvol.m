200855c58902e0ea2720e90766a3939fc29864a0
Perfect Yayra Gidisu <36855498+perfectyayra@users.noreply.github.com>
2022-08-03 19:00:17 +0200

    Update cur_adap_blk_maxvol.m

--- cur_adap_blk_maxvol.m
+++ cur_adap_blk_maxvol.m
@@ -1,12 +1,17 @@
-function irow = cur_adap_blk_maxvol(U,k,p,rho,tol)
+function index = adap_blk_maxvol(U,k,p,rho,tol)
+% U is singular vectors
+% k desired number of indices
+% p block size
+% rho evaluation criteria for switch between standard and block deim
+% tol stopping cirteria for maxvol algorithm 
+% see also deim and block_maxvol
+% Revision date: June 18, 2022
+% (C) Perfect Gidisu, Michiel Hochstenbach 2022
+
 if nargin < 3|| isempty(p), p = 2; end
-if nargin < 4|| isempty(rho), rho = 0.9; end
-if nargin < 5 || isempty(tol), tol = 2e-2; end
+if nargin < 4|| isempty(rho), rho = 0.95; end
+if nargin < 5 || isempty(tol), tol = 1e-2; end
 
-irow = deim_adapt(U, k, p, rho,tol);
-
-
-function index = deim_adapt(U, k, p, rho,tol)
 j = 1; index = zeros(1,k);
 while j <= k
   if j > 1, U(:,j) = U(:,j) - U(:,1:j-1)*(U(index(1:j-1),1:j-1) \ U(index(1:j-1),j)); end
